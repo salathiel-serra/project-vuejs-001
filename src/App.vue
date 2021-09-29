3d0dbaaa38a0cc9f9eda855d98df5e6c8d9898b0
Salathiel Serra <salathiel.serra@clinicarx.com.br>
2021-09-29 00:57:01 -0300

    Definindo método created() para acesso a end-point de API no momento de criação do component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
     <ul>
       <li v-for="image of images">
-        <img :src="image.url" v-bind:alt="image.title">
+        <img :src="image.url" :alt="image.title">
       </li>
     </ul>
     
@@ -16,22 +16,15 @@ export default {
   data() {
     return {
 
-      title: 'Project One With Vue.js',
-      images: [
-        {
-          url: 'https://media.istockphoto.com/photos/illustration-of-generic-compact-car-perspective-view-picture-id1148853697?k=20&m=1148853697&s=612x612&w=0&h=x_IL5R5PF48fTZhuD-1gMHDqstoxyrUfRMH-Oet-7Mk=',
-          title: 'Carro vermelho'
-        },
-        {
-          url: 'https://media.istockphoto.com/photos/illustration-of-generic-compact-car-perspective-view-picture-id1148853697?k=20&m=1148853697&s=612x612&w=0&h=x_IL5R5PF48fTZhuD-1gMHDqstoxyrUfRMH-Oet-7Mk=',
-          title: 'Carro vermelho'
-        },
-        {
-          url: 'https://media.istockphoto.com/photos/illustration-of-generic-compact-car-perspective-view-picture-id1148853697?k=20&m=1148853697&s=612x612&w=0&h=x_IL5R5PF48fTZhuD-1gMHDqstoxyrUfRMH-Oet-7Mk=',
-          title: 'Carro vermelho'
-        }
-      ]
+      title: "Project One With Vue.js",
+      images: []
     }
+  },
+
+  created() {
+    this.$http.get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(images => this.images = images, err => console.log(err));
   }
   
 }
