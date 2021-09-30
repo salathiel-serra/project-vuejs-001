962b8e873886bac6213d370ef11ba441db9d7236
Salathiel Serra <salathiel.serra@clinicarx.com.br>
2021-09-30 01:29:28 -0300

    Entendendo uso do component Transition

--- src/components/shared/panel/Panel.vue
+++ src/components/shared/panel/Panel.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="panel">
         <h2 class="panel-title" @dblclick="isVisible = !isVisible" >{{ title }}</h2>
-        <div class="panel-body" v-show="isVisible">
-            <slot></slot>
-        </div>
+        
+        <transition name="panel-fade">
+            <div class="panel-body" v-show="isVisible">
+                <slot></slot>
+            </div>
+        </transition>
     </div>
 </template>
 
@@ -20,28 +23,36 @@
 </script>
 
 <style scoped>
-  .panel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+    .panel {
+        padding: 0 auto;
+        border: solid 2px grey;
+        display: inline-block;
+        margin: 5px;
+        box-shadow: 5px 5px 10px grey;
+        width: 200px;
+        height: 100%;
+        vertical-align: top;
+        text-align: center;
+    }
 
-  .panel .panel-title {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+    .panel .panel-title {
+        text-align: center;
+        border: solid 2px;
+        background: lightblue;
+        margin: 0 0 15px 0;
+        padding: 10px;
+        text-transform: uppercase;
+    }
 
-  * {
-    box-shadow: 5px 5px 5px;
-  }
+    * {
+        box-shadow: 5px 5px 5px;
+    }
+
+    .panel-fade-enter, .panel-fade-leave-active {
+        opacity: 0
+    }
+
+    .panel-fade-enter-active, .panel-fade-leave-active {
+        transition: opacity .3s
+    }
 </style>
