20879a248d1370663f391d3ea6ce130ab9b94399
Salathiel Serra <salathiel.serra@clinicarx.com.br>
2021-10-03 10:35:55 -0300

    Iniciando implementação de component para cadastro de imagens

---
+++ src/components/registration/Registration.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3 class="text-center"> Formulário </h3>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .text-center {
+        text-align: center;
+    }
+</style>
